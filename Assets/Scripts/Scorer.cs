fdb5dae1fcda659c164ee2d23f775397007deaf1
yosukeadachi <yosukeadachi@gmail.com>
2016-11-22 18:54:21 +0900

    スコアシステム追加

---
+++ Assets/Scripts/Scorer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scorer : MonoBehaviour {
+
+	private int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void AddScore() {
+		score ++;
+		Debug.Log("score " + score);
+	}
+}
